faa36e0d2f4fc720b86eebcb94afff14368bd8d1
Jerome Leclanche <jerome@leclan.ch>
2015-03-28 23:45:52 +0100

    Add a BattleNet backend switcher hook

---
+++ HookRegistry/BackendSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace Hooks
+{
+	[RuntimeHookAttribute]
+	public class BackendSwitcher
+	{
+		public BackendSwitcher()
+		{
+			HookRegistry.Register (OnCall);
+		}
+
+		object OnCall(string typeName, string methodName, params object[] args) {
+			if (typeName != "BattleNet" || methodName != ".cctor") {
+				return null;
+			}
+			string backend = Vars.Key("Aurora.Backend").GetStr("BattleNetDll");
+
+			IBattleNet impl;
+			if (backend == "BattleNetDll") {
+				impl = new BattleNetDll ();
+			} else if (backend == "BattleNetCSharp") {
+				impl = new BattleNetCSharp();
+			} else {
+				throw new NotImplementedException("Invalid Battle.net Backend (Aurora.Backend)");
+			}
+			typeof(BattleNet).GetField("s_impl", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, impl);
+			Log.Bob.Print("Forced BattleNet backend to {0}", backend);
+			return 1;
+		}
+	}
+}
