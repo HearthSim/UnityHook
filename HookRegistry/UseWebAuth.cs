dec60d1a7e3f5d1d059c9aed0d5ba7b88d8a0965
Robert Nix <mischanix@gmail.com>
2015-07-18 21:18:24 -0500

    Added UseWebAuth hook
    This enables responding to an external challenge request on PC,
    by sending the Aurora.VerifyWebCredentials config value as an
    authentication token.

---
+++ HookRegistry/UseWebAuth.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Hooks
+{
+	[RuntimeHook]
+	class UseWebAuth
+	{
+		public UseWebAuth()
+		{
+			HookRegistry.Register(OnCall);
+		}
+
+		object OnCall(string typeName, string methodName, object thisObj, object[] args)
+		{
+			if (typeName != "BattleNetCSharp" || methodName != "AuroraStateHandler_WaitForLogon")
+			{
+				return null;
+			}
+
+			var bnet = (BattleNetCSharp)thisObj;
+			var challenge = typeof(ChallengeAPI).GetField("m_nextExternalChallenge", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(bnet.Challenge) as ExternalChallenge;
+			if (challenge != null)
+			{
+				var url = "";
+				Log.Bob.Print("UseWebAuth: CheckWebAuth returned {0}, {1}", bnet.CheckWebAuth(out url), url);
+			}
+			return null;
+		}
+	}
+}
