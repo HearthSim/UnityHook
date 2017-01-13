da829cc06f4f6b12f35c53bd903a096b80bad88d
Bert Proesmans <bproesmans@hotmail.com>
2017-01-13 23:14:59 +0100

    Added packet dumper

---
+++ HookRegistry/src/packetdumper/IncomingPackets.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Hooks.PacketDumper
+{
+    [RuntimeHook]
+    class IncomingPackets
+    {
+        // Represents bgs.BattleNetPacket
+        Type TypeBattleNetPacket;
+        // Represents PegasusPacket
+        Type TypePegasusPacket;
+
+        public IncomingPackets()
+        {
+            HookRegistry.Register(OnCall);
+            // Load necessary calls
+            PrepareDynamicCalls();
+        }
+
+        private void PrepareDynamicCalls()
+        {
+            // Prepare dynamic call to CSharp-firstpass library
+            // Load from assembly file at currently executing path
+            var locFirstPass = Path.Combine(HookRegistry.LibLocation, HookRegistry.LIB_CSHARP_FIRSTP_NAME);
+            Assembly libFirstPass = Assembly.LoadFrom(locFirstPass);
+            TypeBattleNetPacket = libFirstPass.GetType("bgs.BattleNetPacket");
+
+            var loc = Path.Combine(HookRegistry.LibLocation, HookRegistry.LIB_CSHARP_NAME);
+            Assembly lib = Assembly.LoadFrom(loc);
+            TypePegasusPacket = lib.GetType("PegasusPacket");
+        }
+
+        // Returns a list of methods (full names) that this hook expects. 
+        // The Hooker will cross reference all returned methods with the requested methods.
+        public static string[] GetExpectedMethods()
+        {
+            return new string[] { "bgs.BattleNetPacket.IsLoaded", "PegasusPacket.IsLoaded" };
+        }
+
+        private object ProxyIsLoaded(string typeName, object thisObj)
+        {
+            switch(typeName)
+            {
+                case "bgs.BattleNetPacket":
+                    return TypeBattleNetPacket.GetMethod("IsLoaded").Invoke(thisObj, new object[] { });
+                    break;
+                case "PegasusPacket":
+                    return TypePegasusPacket.GetMethod("IsLoaded").Invoke(thisObj, new object[] { });
+                    break;
+                default:
+                    // Returning false here would just introduce undefined behaviour
+                    HookRegistry.Panic("Unknown typename!");
+                    break;
+            }
+
+            return false;
+        }
+
+        private void DumpPacket(string typeName, object thisObj)
+        {
+            // We only have to trigger the encode method, because that one should be hooked as well!
+            // We can't call encode and dump the returned value, because that leads to multiple dumps of
+            // the same data.
+            switch (typeName)
+            {
+                case "bgs.BattleNetPacket":
+                    TypeBattleNetPacket.GetMethod("Encode").Invoke(thisObj, new object[] { });
+                    break;
+                case "PegasusPacket":
+                    TypePegasusPacket.GetMethod("Encode").Invoke(thisObj, new object[] { });
+                    break;
+                default:
+                    // Returning false here would just introduce undefined behaviour
+                    HookRegistry.Panic("Unknown typename!");
+                    break;
+            }
+        }
+
+
+        object OnCall(string typeName, string methodName, object thisObj, object[] args)
+        {
+            if (typeName != "bgs.BattleNetPacket" || typeName != "PegasusPacket" || methodName != "IsLoaded")
+            {
+                return null;
+            }
+
+            // Call the real method
+            object isLoaded = ProxyIsLoaded(typeName, thisObj);
+
+
+            if((bool)isLoaded == true)
+            {
+                // If the packet is complete, we copy it to our own stream
+                DumpPacket(typeName, thisObj);
+            }
+
+            return isLoaded;
+        }
+    }
+}
---
+++ HookRegistry/src/packetdumper/TeeStream.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Hooks.PacketDumper
+{
+    class TeeStream
+    {
+
+        public const int BNET_LISTEN_PORT = 30123;
+        public const int PEGASUS_LISTEN_PORT = 30124;
+
+        // Stream where all battle.net packets are being written to
+        private TcpClient StreamBattleNet;
+        // Stream where all Pegasus packets are being written to
+        private TcpClient StreamPegasus;
+
+        private TeeStream()
+        { }
+
+        private void Initialise()
+        {
+            try
+            {
+                // Bind to localhost so the packets use the loopback interface to reach the listener.
+                // This prevents sending packets to other devices on the connected network.
+                StreamBattleNet = new TcpClient(); // "localhost", 30123
+                StreamPegasus = new TcpClient(); // "localhost", 30124
+                // Start connection task
+                var bnetTask = StreamBattleNet.BeginConnect("localhost", BNET_LISTEN_PORT, null, null);
+                var pegTask = StreamPegasus.BeginConnect("localhost", PEGASUS_LISTEN_PORT, null, null);
+                // Wait 3 seconds max before timing out the connection attempt
+                var waitSeconds = 3;
+                var bnetSucces = bnetTask.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(waitSeconds));
+                var pegSucces = pegTask.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(waitSeconds));
+
+                if (!bnetSucces || !pegSucces)
+                {
+                    HookRegistry.Get().Log("Creating connection to packetlistener failed!");
+                    StreamBattleNet.Close();
+                    StreamPegasus.Close();
+                    StreamBattleNet = null;
+                    StreamPegasus = null;
+                }
+
+            }
+            catch (SocketException e)
+            {
+                var msg = string.Format("Couldn't open a stream to dump the network packets. Message: `{0}`", e.Message);
+                HookRegistry.Get().Log(msg);
+            }
+        }
+
+        public void WriteBattlePacket(byte[] data)
+        {
+            StreamBattleNet?.GetStream().Write(data, 0, data.Length);
+        }
+
+        public void WritePegasusPacket(byte[] data)
+        {
+            StreamPegasus?.GetStream().Write(data, 0, data.Length);
+        }
+
+        private static TeeStream _thisObj;
+        public static TeeStream Get()
+        {
+            if (_thisObj == null)
+            {
+                _thisObj = new TeeStream();
+                _thisObj.Initialise();
+            }
+
+            return _thisObj;
+        }
+    }
+}
