2c2cf9a220e0c2eda21a4156dfaa864323c025db
Bert Proesmans <bproesmans@hotmail.com>
2017-05-12 00:57:26 +0200

    Removed leading tab from successfully hooked message

--- Hooker/src/hook/Hooker.cs
+++ Hooker/src/hook/Hooker.cs
@@ -12,7 +12,7 @@ namespace Hooker
 		public const string UNEXPECTED_METHOD =
 			"The method `{0}` is not expected by your version of HookRegistry." +
 			"Continuing will have no effect. Please update your hooksfile or retrieve another HookRegistry dll file that hooks into the given method!";
-		public const string HOOKED_METHOD = "\tMethod `{0}` hooked.";
+		public const string HOOKED_METHOD = "Method `{0}` hooked.";
 		public const string HOOK_PROBLEM = "A problem occurred while hooking `{0}`: {1}\n" +
 										   "Hooking will continue!";
 
