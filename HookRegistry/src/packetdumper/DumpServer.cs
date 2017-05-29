bb5ef6017a231b6bf60c37e47af54f08169470db
Bert Proesmans <bproesmans@hotmail.com>
2017-05-29 23:18:53 +0200

    Small updates to packet dumper hooks

---
+++ HookRegistry/src/packetdumper/DumpServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Hooks.src.packetdumper
+{
+	class DumpServer
+	{
+		private TcpListener _connectionListener;
+
+		private MemoryStream _replayBuffer;
+
+
+	}
+}
